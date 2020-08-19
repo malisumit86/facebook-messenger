16e6b8133f6116bfb5e9eb564de5a5a8b7feeaaa
malisumit862 <sumitrm93@gmail.com>
2020-08-19 18:48:30 +0530

    Updates

---
+++ precache-manifest.1c735bb3f6f87ae8dbe08e7cbb750dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c3af10db89d42ae36bea6c672f401b",
+    "url": "/facebook-messenger/index.html"
+  },
+  {
+    "revision": "1cf3d733bfe86594cbd1",
+    "url": "/facebook-messenger/static/css/main.fb2cb030.chunk.css"
+  },
+  {
+    "revision": "120fe55fccd9a2986e0b",
+    "url": "/facebook-messenger/static/js/2.a0c6da4d.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-messenger/static/js/2.a0c6da4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf3d733bfe86594cbd1",
+    "url": "/facebook-messenger/static/js/main.4c3c5299.chunk.js"
+  },
+  {
+    "revision": "f361b87a14f4c7d9ca49",
+    "url": "/facebook-messenger/static/js/runtime-main.9ee3e68e.js"
+  }
+]);
